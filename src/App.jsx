73a6cc248949661f803a0b17cfb7c78b322451ed
Abhi-hertz <93651229+AE-Hertz@users.noreply.github.com>
2024-10-05 16:08:04 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import TicTacToe from './components/TicTacToe/TicTacToe'
+
+function App() {
+
+  return (
+    <>
+     <div>
+        <TicTacToe/>
+     </div>
+    </>
+  )
+}
+
+export default App
